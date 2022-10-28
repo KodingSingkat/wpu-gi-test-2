3102c2972f7381de780eb714335b6a7b3e1e8b66
Koding Singkat <kodingsingkat@gmail.com>
2022-10-28 17:01:45 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+
+<body>
+
+    <h1>Hallo World!</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit laboriosam ullam, eligendi tempora vitae sint cupiditate quae tempore eius illum voluptatem, facilis cum provident ducimus minus, pariatur fuga repellat omnis.</p>
+
+</body>
+
+</html>
